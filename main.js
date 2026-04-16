22d64e3428f7d23afec84f009955ceafd98fd1e5
Christian Tondo <christiantondo1@gmail.com>
2026-04-16 13:37:14 +0200

    snack 5

--- main.js
+++ main.js
@@ -70,6 +70,13 @@ timer();
 // Crea una funzione stampaOgniSecondo con setInterval.
 // Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
 
+function stampaOgniSecondo(msg) {
+    return setInterval(() => {
+        console.log(msg);
+    }, 1000);
+}
+stampaOgniSecondo("Ciao ogni secondo!");
+
 // Snack 6
 // Crea un contatore automatico con setInterval.
 // Definisci una funzione creaContatoreAutomatico che accetta un intervallo di tempo e restituisce una funzione che avvia un setInterval, incrementando un contatore e stampandolo.
